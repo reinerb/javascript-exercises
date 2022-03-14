e4148af6da36b9e58a78ba2c8731285f473b760e
Ben Reiner <benjamin.t.reiner@gmail.com>
2022-03-14 11:52:46 -0400

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (s) {
+  let newString = "";
 
+  for (let i = 0; i < s.length; i++) {
+    newString = s.slice(i, i + 1) + newString;
+  }
+
+  return newString;
 };
 
 // Do not edit below this line
