1a4b51d8d29629fd5c8cd5c595c1391b14cef52c
Ben Reiner <benjamin.t.reiner@gmail.com>
2022-03-14 14:42:58 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacciRecursive = function (n, secondLast, last) {
+  if (n == 0) {
+    return last;
+  } else {
+    return fibonacciRecursive(n - 1, last, last + secondLast);
+  }
+};
 
+const fibonacci = function (n) {
+  if (n <= 0) {
+    return "OOPS";
+  } else if (n == 1 || n == 2) {
+    return 1;
+  } else {
+    return fibonacciRecursive(n - 2, 1, 1);
+  }
 };
 
 // Do not edit below this line
