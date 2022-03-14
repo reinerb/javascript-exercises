3c9f4401cc2ba10f21846f0f407f4b3f9ed2fbf4
Ben Reiner <benjamin.t.reiner@gmail.com>
2022-03-14 12:35:17 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0 || typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";
+  }
 
+  let total = 0;
+
+  for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+    total += i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
