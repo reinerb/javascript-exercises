007d462909eb10abd190c6c79d4f701f852a6b42
Ben Reiner <benjamin.t.reiner@gmail.com>
2022-03-14 13:16:43 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+const alphaOnly = function (s) {
+  let newString = "";
+
+  for (let i = 0; i < s.length; i++) {
+    if (alphabet.indexOf(s.slice(i, i + 1).toLowerCase()) !== -1) {
+      newString = newString + s.slice(i, i + 1);
+    }
+  }
+
+  return newString;
+};
+
+const reverse = function (s) {
+  let reversed = "";
+
+  for (let i = 0; i < s.length; i++) {
+    reversed = s.slice(i, i + 1) + reversed;
+  }
+
+  return reversed;
+};
+
+const palindromes = function (s) {
+  let lettersOnly = alphaOnly(s).toLowerCase();
+  return lettersOnly === reverse(lettersOnly);
 };
 
 // Do not edit below this line
