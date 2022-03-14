6b1bebf794873fe38527e287ab8356eadd4100f7
Ben Reiner <benjamin.t.reiner@gmail.com>
2022-03-14 12:57:52 -0400

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let total = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    total = total + arr[i];
+  }
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function (arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
 
+  let product = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    product = product * arr[i];
+  }
+
+  return product;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let nFactorial = 1;
+  for (let i = 2; i <= n; i++) {
+    nFactorial *= i;
+  }
+
+  return nFactorial;
 };
 
 // Do not edit below this line
@@ -29,5 +50,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
