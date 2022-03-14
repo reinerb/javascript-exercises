3a575f5df76ac8710999f0095d90a4cf3d6f8442
Ben Reiner <benjamin.t.reiner@gmail.com>
2022-03-14 11:50:01 -0400

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (s, n) {
+  if (n < 0) {
+    return "ERROR";
+  }
 
+  newString = "";
+
+  for (let i = 0; i < n; i++) {
+    newString = newString + s;
+  }
+
+  return newString;
 };
 
 // Do not edit below this line
