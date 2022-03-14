d3ff674ea87f1bfbf96ecc5eeb17789e0d65b035
Ben Reiner <benjamin.t.reiner@gmail.com>
2022-03-14 15:58:00 -0400

    Complete exercise 13

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const CURRENT_YEAR = 2022;
 
+const findTheOldest = function (people) {
+  let maxAge = 0;
+  let maxAgeIndex = -1;
+
+  for (let i = 0; i < people.length; i++) {
+    if (!people[i]["yearOfDeath"] && people[i]["yearOfDeath"] !== 0) {
+      age = CURRENT_YEAR - people[i]["yearOfBirth"];
+    } else {
+      age = people[i]["yearOfDeath"] - people[i]["yearOfBirth"];
+    }
+
+    if (maxAgeIndex == -1 || age > maxAge) {
+      maxAge = age;
+      maxAgeIndex = i;
+    }
+  }
+
+  return people[maxAgeIndex];
 };
 
 // Do not edit below this line
