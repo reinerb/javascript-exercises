d0526d1c20fef857346ca6afe3c023af61f39da1
Ben Reiner <benjamin.t.reiner@gmail.com>
2022-03-14 12:24:21 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  let currentArray = Array.from(arguments[0]);
+  let newArray = Array();
+  let valuesToRemove = Array.from(arguments).slice(1);
 
+  for (let i = 0; i < currentArray.length; i++) {
+    if (valuesToRemove.indexOf(currentArray[i]) === -1) {
+      newArray.push(currentArray[i]);
+    }
+  }
+
+  return newArray;
 };
 
 // Do not edit below this line
